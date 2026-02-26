675b79b9b4e35f9cb83dd6e39ebacf6d720bcefa
Sean Craig <seanwhcraig@gmail.com>
2026-02-26 09:27:15 -0600

    Chaged colors

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -5,9 +5,9 @@ import Button from '../Button/Button';
 
 function PricingCard({ title, priceAmount, pricePeriod, includesText, features, buttonText, buttonVariant }) {
     return (
-        <div className="flex flex-col min-h-[380px] bg-card rounded p-4 w-80 shrink-0">
+        <div className="flex flex-col min-h-[380px] bg-card p-4 w-80 shrink-0">
             <div className="flex flex-col">
-                <h3 className="text-[22px] leading-7 font-normal text-text-primary mb-2">
+                <h3 className="text-[22px] leading-7 font-medium text-text-primary mb-2">
                     {title}
                 </h3>
 
@@ -37,3 +37,4 @@ function PricingCard({ title, priceAmount, pricePeriod, includesText, features, 
 }
 
 export default PricingCard;
+ 
